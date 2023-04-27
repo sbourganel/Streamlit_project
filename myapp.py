e97df14783a119353dfa20cc41853d1195b21196
sbourganel <63141771+sbourganel@users.noreply.github.com>
2023-04-27 16:50:30 +0200

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -1,10 +1,36 @@
 import streamlit as st
-import pandas as pd
-
-st.title("Ma première application Streamlit")
-
-st.write("Bienvenue dans cette application! Voici un exemple d'image:")
-age = st.slider('Quel est votre âge?', 0, 100, 25)
-st.write("Vous avez", age, "ans")
 
 
+st.title('Outil de recommandation de films')
+
+  
+  
+st.write("Please select what kind of movie, you wanna watch")
+# multi select box
+ 
+# first argument takes the box title
+# second argument takes the options to show
+genres = st.multiselect("Genres: ",
+                         ['Drama',
+                          'Action', 
+                          'Fantasy',
+                          'Comedy',
+                          'Romance', 'Thriller', 'Crime', 'Adventure', 'Horror', 'Mystery', 'Biography', 'Family', 'History', 'Animation','Sci-Fi', 'Documentary'])
+
+
+
+# write the selected options
+st.write("You selected", len(genres), 'genres')
+
+
+# slider
+ 
+# first argument takes the title of the slider
+# second argument takes the starting of the slider
+# last argument takes the end number
+level = st.slider("Sélectionnez une note minimale de film", 1, 10)
+ 
+# print the level
+# format() is used to print value
+# of a variable at a specific position
+st.text('Selected: {}'.format(level))
